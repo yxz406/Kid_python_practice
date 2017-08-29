b1a4605de5244d24fb6a168260cf355b501cfa68
yxz <yxz406@163.com>
2017-08-29 11:30:41 -0400

    add p18 practice files

---
+++ p18_2.py
@@ -0,0 +1,36 @@
+import pygame, sys
+pygame.init()
+delay = 100
+interval = 50
+pygame.key.set_repeat(delay, interval)
+screen = pygame.display.set_mode([1024, 720])
+background = pygame.Surface(screen.get_size())
+background.fill([255,255,255])
+clock = pygame.time.Clock()
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, image_file, location, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+        self.speed = speed
+    def move(self):
+        if self.rect.left <= screen.get_rect().left or \
+            self.rect.right >= screen.get_rect().right:
+            self.speed[0] = -self.speed[0]
+        newpos = self.rect.move(self.speed)
+        self.rect = newpos
+my_ball = Ball("ball.jpg", [10,0],[20,20])
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEMOTION:
+            my_ball.rect.center = event.pos
+    clock.tick(30)
+    screen.blit(background, (0,0))
+    my_ball.move()
+    screen.blit(my_ball.image, my_ball.rect)
+    pygame.display.flip()
+pygame.quit()
